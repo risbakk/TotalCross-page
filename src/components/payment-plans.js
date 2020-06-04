3badff9b8df0f3527d1d34cedc0b6e8365dbbdcb
Nathalie Marquesin <nmarquesin@live.com>
2020-06-04 23:14:03 +0100

    Design edit

--- src/components/payment-plans.js
+++ src/components/payment-plans.js
@@ -8,67 +8,80 @@ import { CONTACT, GET_STARTED } from "../utils/links";
 const check = <FontAwesomeIcon className={styles.icon} icon={faCheckCircle} />;
 
 const PaymentPlans = () => {
+  const plans = [
+    {
+      features: [
+        "Unlimited Developers",
+        "Unlimited Runtime",
+        "Community Support",
+        "Embedded Apps: Linux Arm, Android, WinCE",
+        "Desktop Apps: Windows, Linux",
+        "Mobile Apps: Android, iOS",
+        "Documentation and Guidelines",
+        "Video Tutorials",
+        "Forum",
+      ],
+      title: "Free",
+      license: "Under LGPL v2.1",
+      button: {
+        text: "Get Started",
+        url: GET_STARTED,
+        style: "primary",
+      },
+    },
+    {
+      features: [
+        "01 Developer per License",
+        "Unlimited Runtime",
+        "Premium Support",
+        "Embedded Apps: Linux Arm, Android, WinCE",
+        "Desktop Apps: Windows, Linux",
+        "Mobile Apps: Android, iOS",
+        "Documentation and Guidelines",
+        "Video Tutorials",
+        "Forum",
+      ],
+      title: "Enterprise",
+      license: "",
+      button: {
+        text: "Contact Us",
+        url: CONTACT,
+        style: "secondary",
+      },
+    },
+  ];
   return (
     <div className={styles.container}>
       <h1 className={styles.pageTitle}>Our Plans</h1>
       <div className={styles.cardsContainer}>
-        <div className={styles.card}>
-          <h3 className={styles.cardTitle}>Free</h3>
-          <h5 className={styles.cardSubtitle}>Under LGPL v2.1</h5>
-          <ul className={styles.cardList}>
-            <li className={styles.cardListItem}>
-              {check}
-              Unlimited Developers
-            </li>
-            <li className={styles.cardListItem}>{check} Unlimited Runtime</li>
-            <li className={styles.cardListItem}>{check} Community Support</li>
-            <li className={styles.cardListItem}>
-              {check} Embedded Apps: Linux Arm, Android, WinCE
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Desktop Apps: Windows, Linux
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Mobile Apps: Android, iOS
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Documentation and Guidelines
-            </li>
-            <li className={styles.cardListItem}>{check} Video Tutorials</li>
-            <li className={styles.cardListItem}>{check} Forum</li>
-          </ul>
-          <a href={GET_STARTED} target="_blank" rel="noreferrer">
-            <button className={styles.cardButtonGetStarted}>Get Started</button>
-          </a>
-        </div>
-        <div className={styles.card}>
-          <h3 className={styles.cardTitle}>Enterprise</h3>
-          <h5 className={styles.cardSubtitle}></h5>
-          <ul className={styles.cardList}>
-            <li className={styles.cardListItem}>
-              {check} 01 Developer per License
-            </li>
-            <li className={styles.cardListItem}>{check} Unlimited Runtime</li>
-            <li className={styles.cardListItem}>{check} Premium Support</li>
-            <li className={styles.cardListItem}>
-              {check} Embedded Apps: Linux Arm, Android, WinCE
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Desktop Apps: Windows, Linux
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Mobile Apps: Android, iOS
-            </li>
-            <li className={styles.cardListItem}>
-              {check} Documentation and Guidelines
-            </li>
-            <li className={styles.cardListItem}>{check} Video Tutorials</li>
-            <li className={styles.cardListItem}>{check} Forum</li>
-          </ul>
-          <Link to={CONTACT}>
-            <button className={styles.cardButtonContact}>Contact Us</button>
-          </Link>
-        </div>
+        {plans.map((plan) => (
+          <div className={styles.card}>
+            <h3 className={styles.cardTitle}>{plan.title}</h3>
+            <h5 className={styles.cardSubtitle}>{plan.license}</h5>
+            <ul className={styles.cardList}>
+              {plan.features.map((item) => (
+                <li className={styles.cardListItem}>
+                  <div className={styles.bulletPoint}>
+                    <div className={styles.bulletPointBackground}></div>
+                    <div className={styles.bulletPointIcon}>{check}</div>
+                  </div>
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <a href={plan.button.url} target="_blank" rel="noreferrer">
+              {plan.button.style === "primary" ? (
+                <button className={styles.cardButtonGetStarted}>
+                  {plan.button.text}
+                </button>
+              ) : (
+                <button className={styles.cardButtonContact}>
+                  {plan.button.text}
+                </button>
+              )}
+            </a>
+          </div>
+        ))}
       </div>
     </div>
   );
